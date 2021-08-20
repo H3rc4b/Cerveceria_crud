94e3125c26247667b7b7cdcef8e7dfdbb5dac44b
hernan caballero <hernan_1587@hotmail.com>
2021-08-20 11:59:24 -0300

    actualizacion de la base de datos

--- DB Caballero/cerveceria.sql
+++ DB Caballero/cerveceria.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 11-08-2021 a las 03:47:06
+-- Tiempo de generación: 20-08-2021 a las 14:58:12
 -- Versión del servidor: 5.5.24-log
 -- Versión de PHP: 5.4.3
 
@@ -31,16 +31,20 @@ CREATE TABLE IF NOT EXISTS `novedades` (
   `titulo` varchar(250) NOT NULL,
   `subtitulo` text NOT NULL,
   `cuerpo` text NOT NULL,
+  `img_id` varchar(250) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=14 ;
 
 --
 -- Volcado de datos para la tabla `novedades`
 --
 
-INSERT INTO `novedades` (`id`, `titulo`, `subtitulo`, `cuerpo`) VALUES
-(1, 'La vacuna Sinopharm está entre las candidatas para inocular niños y adolescentes de 3 a 17 en la Argentina', 'La ministra de Salud, Carla Vizzotti, se reunió hoy con investigadores de los Emiratos Árabes Unidos por el uso de emergencia de la vacuna china en niños y adolescentes. Aún el productor chino no publicó la información científica respaldatoria en menores\r\n10 de Agosto de 2021', 'El plan de vacunación a nivel nacional avanza en lograr inmunizaciones en las franjas etarias más bajas. Sin embargo, la mayoría de los productores de vacunas contra el COVID-19 no incluyeron en un principio como grupos priorizados a las niñas, niños y adolescentes -comprendidos entre 17 a 3 años, ya que ellos hasta la evidencia actual desarrollan el COVID-19 con cuadros leves; y sobre todo porque se necesitan ensayos clínicos específicos para esos grupos etarios.\r\n\r\nSin embargo, cada vez hay más consenso entre los especialistas de caminar hacia sociedades plenamente vacunadas, y acerca de que los menores de 18 representan un segmento etario fundamental para colaborar en la tan mentada inmunidad de rebaño. Queda claro entonces que los niños y jóvenes inmunizados colaborarán a “perforar” los contagios y la aparición de nuevas variantes. Es cierto también que los adolescentes y los niños hoy representan el segmento etario con menos incidencia en la enfermedad COVID-19, pero los mismos especialistas advirtieron sobre la necesidad de incluir a los niños, a pesar de que sus posibilidades de enfermar gravemente y morir fue estimada en 1 en 500.000.'),
-(2, 'Messi ya brilla en PSG: el repaso de una jornada histórica', 'Leo revolucionó en su llegada a París. En Olé te mostramos, con el mejor material multimedia, todo lo sucedido lo largo del día.', 'El mundo está revolucionado, con el epicentro de la cuestión en París. Porque Lionel Messi llegó este martes a la capital francesa para convertirse en nuevo jugador de PSG. Un día histórico para todos. Después de casi 17 años como profesional, Leo dejó, con dolor, Barcelona para unirse al equipo que comanda actualmente Mauricio Pochettino, plagado de otras estrellas y con altísimas ambiciones para esta temporada. A continuación, en Olé, vamos a repasar -con amplio contenido audiovisual- todo lo que fue el día de nuestro 10, en una jornada maratónica. Y seguirá este miércoles, con su presentación formal ante la prensa.');
+INSERT INTO `novedades` (`id`, `titulo`, `subtitulo`, `cuerpo`, `img_id`) VALUES
+(1, 'La vacuna Sinopharm está entre las candidatas para inocular niños y adolescentes de 3 a 17 en la Argentina', 'La ministra de Salud, Carla Vizzotti, se reunió hoy con investigadores de los Emiratos Árabes Unidos por el uso de emergencia de la vacuna china en niños y adolescentes. Aún el productor chino no publicó la información científica respaldatoria en menores\r\n10 de Agosto de 2021', 'El plan de vacunación a nivel nacional avanza en lograr inmunizaciones en las franjas etarias más bajas. Sin embargo, la mayoría de los productores de vacunas contra el COVID-19 no incluyeron en un principio como grupos priorizados a las niñas, niños y adolescentes -comprendidos entre 17 a 3 años, ya que ellos hasta la evidencia actual desarrollan el COVID-19 con cuadros leves; y sobre todo porque se necesitan ensayos clínicos específicos para esos grupos etarios.\r\n\r\nSin embargo, cada vez hay más consenso entre los especialistas de caminar hacia sociedades plenamente vacunadas, y acerca de que los menores de 18 representan un segmento etario fundamental para colaborar en la tan mentada inmunidad de rebaño. Queda claro entonces que los niños y jóvenes inmunizados colaborarán a “perforar” los contagios y la aparición de nuevas variantes. Es cierto también que los adolescentes y los niños hoy representan el segmento etario con menos incidencia en la enfermedad COVID-19, pero los mismos especialistas advirtieron sobre la necesidad de incluir a los niños, a pesar de que sus posibilidades de enfermar gravemente y morir fue estimada en 1 en 500.000.', NULL),
+(2, 'Messi ya brilla en PSG: el repaso de una jornada histórica', 'Leo revolucionó en su llegada a París. En Olé te mostramos, con el mejor material multimedia, todo lo sucedido lo largo del día.', 'El mundo está revolucionado, con el epicentro de la cuestión en París. Porque Lionel Messi llegó este martes a la capital francesa para convertirse en nuevo jugador de PSG. Un día histórico para todos. Después de casi 17 años como profesional, Leo dejó, con dolor, Barcelona para unirse al equipo que comanda actualmente Mauricio Pochettino, plagado de otras estrellas y con altísimas ambiciones para esta temporada. A continuación, en Olé, vamos a repasar -con amplio contenido audiovisual- todo lo que fue el día de nuestro 10, en una jornada maratónica. Y seguirá este miércoles, con su presentación formal ante la prensa.', 'oa5g3eqjxnosnerotq9k'),
+(10, 'hernan', 'CABALLERO', 'scvfv', 'vosykpr8bld1j05t77id'),
+(12, 'prueba18', 'prueba18', 'prueba18', NULL),
+(13, 'maradona', 'maradona', 'maradona', 'k4vtslbsvjjhw62q27h2');
 
 -- --------------------------------------------------------
 
@@ -53,7 +57,7 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `usuario` varchar(250) NOT NULL,
   `password` varchar(250) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
 
 --
 -- Volcado de datos para la tabla `usuarios`
